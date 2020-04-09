2d3e9d927b9874a5c12764d219bc520083d16ea1
Wouter <wouterpotters@users.noreply.github.com>
2020-04-09 23:20:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
             'pandas',
             'requests',
             'progressbar',
-            'logging',
+            'logging'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
