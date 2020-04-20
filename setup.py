5fbd2ecbb4a2111dfc5c0e9426376eea4c0c48ab
Wouter <wouterpotters@users.noreply.github.com>
2020-04-20 17:21:28 +0200

    Removed logging from install_requires
    Not required in python3

--- setup.py
+++ setup.py
@@ -13,9 +13,8 @@ setup(
   install_requires=[            # I get to this in a second
             'pandas',
             'requests',
-            'progressbar',
-            'logging'
-      ],
+            'progressbar'
+  ],
   long_description=open('README.md').read(),
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
