9b59d2ad04f4a2469b91156f836ed79796915f93
Wouter <wouterpotters@users.noreply.github.com>
2020-04-20 17:49:25 +0200

    Added version requirements
     for progressbar2 and other package

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ setup(
   download_url = 'https://github.com/wouterpotters/castorapi/archive/master.zip',    # I explain this later on
   keywords = ['Castor EDC', 'API', 'Castor','Clinical study','database','data science'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-            'pandas',
-            'requests',
-            'progressbar'
+            'pandas>=1.0',
+            'requests>=2.23',
+            'progressbar2>=3.5'
   ],
   long_description=open('README.md').read(),
   classifiers=[
