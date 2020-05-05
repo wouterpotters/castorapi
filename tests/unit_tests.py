3179e54ecf68c95a90d3c88831233d353accf164
Wouter Potters <wouter@pottersweb.nl>
2020-05-05 19:43:05 +0200

    Bugfix test for unit test

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -23,6 +23,7 @@ class TestCastorApi(unittest.TestCase):
         # The secret and client id are stored in github secrets
         client_id = os.getenv('castor_clientid')
         client_secret = os.getenv('castor_secret')
+        [print(w) for w in client_id]
         self.c = ca.CastorApi(client_id=client_id,
                               client_secret=client_secret)
 
