986dfb356b37601731dfecc0c56e875b72726965
Wouter <wouterpotters@users.noreply.github.com>
2022-07-26 09:38:06 +0200

    Updated
    Updated building manual for anaconda package

--- conda.sh
+++ conda.sh
@@ -6,9 +6,10 @@ anaconda login
 # Build and publish to https://anaconda.org/wouterpotters/castorapi:
 cd ~/to/folder/with/castorapi_conda/folder
 conda-build castorapi_conda
+conda-build --python 3.10 castorapi_conda
+conda-build --python 3.9 castorapi_conda
+conda-build --python 3.8 castorapi_conda
 conda-build --python 3.7 castorapi_conda
-conda-build --python 3.6 castorapi_conda
-conda-build --python 3.5 castorapi_conda
 
 conda convert --platform all ~/opt/anaconda3/conda-bld/osx-64/castorapi*.bz2 -o ~/opt/anaconda3/conda-bld/
 anaconda upload --skip-existing ~/opt/anaconda3/conda-bld/*/castorapi*.bz2
